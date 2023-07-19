99ee6d460d0a8aef3e22c871c418a6e237e196c0
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-07-19 15:50:05 +0300

    add lib env

--- controller/productsControllers.js
+++ controller/productsControllers.js
@@ -1,5 +1,7 @@
 const { ctrlWrapper } = require("../utils");
 
+require("dotenv").config();
+
 const axios = require("axios");
 const xml2js = require("xml2js");
 
@@ -7,9 +9,10 @@ const util = require("util");
 const parseStringPromise = util.promisify(xml2js.parseString);
 // const { query } = require("express");
 
+const { YML_URL } = process.env;
+
 const getProducts = async (req, res) => {
-  const url =
-    "https://brovko.salesdrive.me/export/yml/export.yml?publicKey=aYcKPKGzZgqwrGsQ2bXXErzE4d8T-YXxJOmKKoayHI84wLVjBm1h_LWz632yguyQ3S07J9mdTZx";
+  const url = YML_URL;
   //   try {
   //     const response = await axios.get(url);
   //     const xmlData = response.data;
