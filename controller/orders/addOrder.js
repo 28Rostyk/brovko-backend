f8204b1a80b71f50830892564e0416a0b3f12e7e
Rostyslav <107403659+28Rostyk@users.noreply.github.com>
2023-08-15 17:48:30 +0300

    Update addOrder.js

--- controller/orders/addOrder.js
+++ controller/orders/addOrder.js
@@ -28,15 +28,44 @@ const { ctrlWrapper } = require("../../utils");
 // };
 
 const addOrder = async (req, res) => {
-  const webhookData = req.body; // Отримайте всі дані вебхуку
+  const webhookData = req.body;
 
   try {
-    const newOrder = await Order.create(webhookData); // Збережіть дані в базі даних
-    res.status(201).json(newOrder); // Поверніть створений об'єкт у відповідь
+    const orderId = webhookData.id;
+    const updatedData = webhookData.data;
+
+    // Знайти заявку за ID
+    let existingOrder = await Order.findOne({ id: orderId });
+
+    if (!existingOrder) {
+      // Якщо заявка не знайдена, то створити нову заявку
+      existingOrder = new Order({
+        id: orderId,
+        data: updatedData,
+      });
+
+      await existingOrder.save();
+      return res.status(201).json(existingOrder);
+    }
+
+    // Якщо заявка знайдена, оновити дані
+    existingOrder.data = { ...existingOrder.data, ...updatedData };
+    await existingOrder.save();
+
+    res.status(200).json(existingOrder);
   } catch (error) {
-    console.error("Помилка під час збереження даних:", error);
+    console.error("Помилка при обробці вебхуку:", error);
     res.status(500).json({ error: "Виникла помилка" });
   }
+  // const webhookData = req.body; // Отримайте всі дані вебхуку
+
+  // try {
+  //   const newOrder = await Order.create(webhookData); // Збережіть дані в базі даних
+  //   res.status(201).json(newOrder); // Поверніть створений об'єкт у відповідь
+  // } catch (error) {
+  //   console.error("Помилка під час збереження даних:", error);
+  //   res.status(500).json({ error: "Виникла помилка" });
+  // }
 };
 
 module.exports = {
