cacb90cef3010287673702cd493a7fbe6646e9d4
Rostyslav <107403659+28Rostyk@users.noreply.github.com>
2023-09-26 13:33:23 +0300

    Update autoFetchProducts.js

--- services/autoFetchProducts.js
+++ services/autoFetchProducts.js
@@ -2,9 +2,9 @@ const axios = require("axios");
 const xml2js = require("xml2js");
 const { Products } = require("../models/products");
 
-const { YML_FILE_TEST } = process.env;
+const { YML_FILE } = process.env;
 
-const ymlFilePath = YML_FILE_TEST;
+const ymlFilePath = YML_FILE;
 
 console.log(ymlFilePath);
 
