7def1681da247baf689be59f5de57248790be94a
Rostyslav <107403659+28Rostyk@users.noreply.github.com>
2023-10-25 22:08:46 +0300

    Update validateBody.js

--- schemas/validateBody.js
+++ schemas/validateBody.js
@@ -2,8 +2,6 @@ const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   const func = async (req, res, next) => {
-    console.log(req.body);
-
     const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, error.message));
