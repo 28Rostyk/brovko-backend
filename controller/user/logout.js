d949e1fc75fe26a451516ca573ff04ce0af79505
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2023-11-14 00:22:55 +0100

    add user orders history

--- controller/user/logout.js
+++ controller/user/logout.js
@@ -2,7 +2,6 @@ const { User } = require("../../models");
 
 const logout = async (req, res) => {
   const { _id } = req.user;
-  console.log("req.user", req.user);
   await User.findByIdAndUpdate(_id, { accessToken: "", refreshToken: "" });
 
   res.status(204).json({
