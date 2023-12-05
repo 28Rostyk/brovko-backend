8b7417eee53811d46d7c95b22cb773c24c9def63
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2023-12-06 00:02:48 +0100

    add foto for reviews

--- models/reviews.js
+++ models/reviews.js
@@ -14,6 +14,10 @@ const reviewsSchema = new Schema(
                 type: String,
                 required: [true, "Text is required"],
               },
+              reviewURL: {
+                type: String,
+                default: "",
+              },
               createdAt: {
                 type: Date,
                 default: Date.now,
