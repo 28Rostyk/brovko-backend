78d0098d30ac13537683c08be4ec6b22b5e34663
Rostyk Podolets <ros.podolets@gmail.com>
2024-01-11 13:38:00 +0200

    Add-product function updated

--- services/createProduct.js
+++ services/createProduct.js
@@ -2,12 +2,15 @@ const axios = require("axios");
 const { PRODUCT_PATH } = process.env;
 
 async function createProduct(body) {
+  // console.log("body into CreateProject :>> ".bgYellow, body.product[0]);
   try {
     const url = PRODUCT_PATH;
     const headers = { "Content-Type": "application/json" };
     const { id, name } = body.product[0];
 
-    const response = await axios.post(url, body, { headers });
+    const bodyToSend = JSON.stringify(body);
+
+    const response = await axios.post(url, bodyToSend, { headers });
 
     return {
       message: `Товар "${name}" з ID(${id}) успішно доданий до SalesDrive.`,
