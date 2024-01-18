625aee86b71bb7652e40add1433a7c656a4a4f6b
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2024-01-19 00:00:57 +0100

    add cloudinary for new product

--- controller/products/addProduct.js
+++ controller/products/addProduct.js
@@ -1,17 +1,44 @@
 const { ctrlWrapper } = require("../../helpers");
 const { createProduct } = require("../../services");
 const { updateDatabase } = require("../../services/updateDatabase");
-// const { changeImage } = require("../../services");
+const { sendInCloudinary } = require("../../services");
+const { clearTemp } = require("../../services");
 
 const addProduct = async (req, res) => {
-  // const body = await changeImage(req.body);
-
-  const update = req.query.update;
-
   try {
-    const response = await createProduct(JSON.parse(req.body.requestBody));
+    const requestBodyObject = JSON.parse(req.body.requestBody);
+    const productURLs = [];
+
+    if (req.files && req.files.length > 0) {
+      for (const item of req.files) {
+        if (item instanceof Object) {
+          const { path: tempUpload, filename } = item;
+          const cloudinaryResponse = await sendInCloudinary(
+            tempUpload,
+            filename
+          );
+          productURLs.push(cloudinaryResponse.secure_url);
+        } else if ("url" in item) {
+          console.log("item.url", item.url);
+          productURLs.push(item.url);
+        }
+      }
+    }
+
+    if (productURLs.length) {
+      const imagesWithFullsize = productURLs.map((url, index) => ({
+        fullsize: url,
+        thumbnail: index,
+      }));
+      requestBodyObject.product[0].images = imagesWithFullsize;
+    }
+
+    const update = req.query.update;
+
+    const response = await createProduct(requestBodyObject);
 
     await updateDatabase(update);
+    await clearTemp();
     res.status(201).json({
       status: response.data.status,
       message: response.message,
