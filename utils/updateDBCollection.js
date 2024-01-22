7bcf46d957110d9b3da16a69ab925b65ffac3696
Rostyk Podolets <ros.podolets@gmail.com>
2024-01-22 19:06:16 +0200

    DB updating function edited

--- utils/updateDBCollection.js
+++ utils/updateDBCollection.js
@@ -1,21 +1,18 @@
-const { Reviews } = require("../models");
+// const { Reviews } = require("../models");
 
 // зразок оновлень :
-const reviewUpdates = {
-  "comments.$[].text.$[].status": {
-    approved: true,
-    approvedBy: { userId: "653f9dd2a3ad1f8599a1c3a9" },
-    approvedAt: new Date(),
-  },
-};
+// const reviewUpdates = {
+//   "comments.$[].text.$[].status": {
+//     approved: true,
+//     approvedBy: { userId: "653f9dd2a3ad1f8599a1c3a9" },
+//     approvedAt: new Date(),
+//   },
+// };
 
 async function addDataToDBCollection(Model, updates) {
   try {
     // Оновлення всіх документів у колекції
-    const { modifiedCount, matchedCount } = await Reviews.updateMany(
-      {},
-      reviewUpdates
-    );
+    const { modifiedCount, matchedCount } = await Model.updateMany({}, updates);
 
     console.log("Updated documents :".bgBlue, `${modifiedCount}`.brightBlue);
     console.log("Matched documents :".bgBlue, `${matchedCount}`.brightBlue);
