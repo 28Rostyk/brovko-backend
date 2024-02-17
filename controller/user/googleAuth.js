341159233ba16ba71b5cde2513f37814a4478b3c
Rostyslav <107403659+28Rostyk@users.noreply.github.com>
2024-02-17 10:12:19 +0200

    Update googleAuth.js

--- controller/user/googleAuth.js
+++ controller/user/googleAuth.js
@@ -2,7 +2,13 @@ const jwt = require("jsonwebtoken");
 
 const { User } = require("../../models");
 
-const { ACCESS_SECRET_KEY, REFRESH_SECRET_KEY, FRONTEND_URL } = process.env;
+const {
+  ACCESS_SECRET_KEY,
+  REFRESH_SECRET_KEY,
+  FRONTEND_URL,
+  ACCESS_TOKEN_LIFE,
+  REFRESH_TOKEN_LIFE,
+} = process.env;
 
 const googleAuth = async (req, res) => {
   const { _id: id } = req.user;
@@ -11,9 +17,11 @@ const googleAuth = async (req, res) => {
     id,
   };
 
-  const accessToken = jwt.sign(payload, ACCESS_SECRET_KEY, { expiresIn: "2d" });
+  const accessToken = jwt.sign(payload, ACCESS_SECRET_KEY, {
+    expiresIn: ACCESS_TOKEN_LIFE,
+  });
   const refreshToken = jwt.sign(payload, REFRESH_SECRET_KEY, {
-    expiresIn: "7d",
+    expiresIn: REFRESH_TOKEN_LIFE,
   });
   await User.findByIdAndUpdate(id, { accessToken, refreshToken });
   res.redirect(
