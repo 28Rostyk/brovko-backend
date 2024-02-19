45062be46a9860a1b5171b21efd86f9f4c096ba7
Rostyk Podolets <ros.podolets@gmail.com>
2024-02-19 12:26:20 +0200

    User fullname detecting added

--- controller/user/userUpdate.js
+++ controller/user/userUpdate.js
@@ -9,9 +9,12 @@ const userUpdate = async (req, res) => {
     throw HttpError(401, "User is not found");
   }
 
+  const body = req.body;
+  body.name = body.firstName + " " + body.lastName;
+
   const updatedUser = await User.findByIdAndUpdate(
     user._id,
-    { ...req.body },
+    { ...body },
     { new: true }
   );
 
