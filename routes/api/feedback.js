6ad0a5d4a6fcf4f6d6f01151727a05d499f57535
Rostyk Podolets <ros.podolets@gmail.com>
2024-02-20 11:06:55 +0200

    Feedback Model is updated

--- routes/api/feedback.js
+++ routes/api/feedback.js
@@ -5,7 +5,7 @@ const { addFeedback, getFeedback } = require("../../controller");
 
 // const { authenticate } = require("../../middlewares");
 
-feedbackRouter.post("/add-feedback", addFeedback);
 feedbackRouter.get("/", getFeedback);
+feedbackRouter.post("/add-feedbacks", addFeedback);
 
 module.exports = { feedbackRouter };
